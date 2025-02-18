13de6aeb6c10e71b95932b9ed8b98fb854b46ee7
Marcos Mendonca <marcosdemendonca@gmail.com>
2025-02-18 09:57:23 -0300

    Update transcricao_youtube.py

--- transcricao_youtube.py
+++ transcricao_youtube.py
@@ -57,6 +57,7 @@ def transcribe():
 
 if __name__ == '__main__':
     import os
-
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))  # Render usa portas acima de 5000
     app.run(host="0.0.0.0", port=port, debug=True)
+
+
